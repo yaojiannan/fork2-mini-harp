c2fbb81de6a4bab24720969436cf5308d448166c
yaojiannan <Yaojiannan@Yaojiannans-MacBook-Pro.local>
2014-05-07 23:14:59 +0900

    second

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
+var makeJade = require('./lib/processor/jade.js');
+var makeLess = require('./lib/processor/less.js');
+var path = require('path');
 
 module.exports = function(dir){
   var app = connect();
+  var jade = makeJade(dir);
+  var less = makeLess(dir);
 
   app.use(function(request,response,next) {
-  	var url = request.url;
-  	if (url == "/current-time") {
-  		response.end((new Date()).toISOString());
+  	if (request.url == "/") {
+  		request.url = "/index.html";
+  	}
+  	next();
+  })
+  app.use(serveStatic(dir));
+
+  app.use(function(request,response,next) {
+  	extname = path.extname(request.url);
+  	if (extname == '.less' || extname == '.jade') {
+  		response.statusCode = 404;
+  		response.end();
   	} else {
   		next();
   	}
   })
-  app.use(serveStatic(dir));
+  
+  app.use(jade);
+  app.use(less);
   return app;
 }
